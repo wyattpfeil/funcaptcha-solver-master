3dfe2e913976e93ed794fb493def9f27382548c4
Wyatt Pfeil <wyatt.pfeil@gmail.com>
2019-12-07 13:49:15 -0500

    Pulls directly from source captcha image

--- GetDegrees.py
+++ GetDegrees.py
@@ -11,8 +11,9 @@ import os
 
 categories = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180]
 
-sourceImageFileName = "vouhgnnkug110"
+sourceImageFileName = "TestIm4"
 sourceImagePath = "./TestingImgsComplete/" + sourceImageFileName + ".png"
+threshImagePath = "./TestingImgsComplete/" + sourceImageFileName + "-Thresh.png"
 resizedImagePath = "./TestingImgsComplete/" + sourceImageFileName + "-Resized.png"
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -25,8 +26,19 @@ model = tensorflow.keras.models.load_model('./converted_keras/keras_model.h5')
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, 224, 224, 3 ), dtype=np.float32)
 
-image = cv2.imread(sourceImagePath)
+def colorToThresh():
+    image = cv2.imread(sourceImagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
+    thresh = thresh[80:195, 93:208]
+    cv2.imwrite(threshImagePath, thresh)
+
+colorToThresh()
+
+image = cv2.imread(threshImagePath)
+
 image = cv2.resize(image, (224, 224))
+
 cv2.imwrite(resizedImagePath, image)
 
 
@@ -53,3 +65,4 @@ print(categories[confidentIndex])
 print(prediction[0][confidentIndex])
 
 os.remove(resizedImagePath)
+os.remove(threshImagePath)
